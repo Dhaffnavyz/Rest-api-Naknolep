42051143a1b836a42b8aae7ee11d58018ee55869
DHAFF OFFICIAL <103863637+Dhaffnavyz@users.noreply.github.com>
2022-07-09 10:25:47 +0700

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -8,7 +8,11 @@ router.get('/', (req, res) => {
 })
 
 router.get('/api', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
+   
+     res.sendFile(__path + '/views/index.html')
+})
+router.get('/pingpong', (req, res) => {
+    res.sendFile(__path + '/views/pingpong.html')
 })
 
 router.get('/api/about', (req, res) => {
